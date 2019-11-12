b8fdf468f4aedbab9f655019876f6d7aebbfe3fa
shgayle <shgayle@gmail.com>
2019-11-12 11:59:08 -0500

    Testing

---
+++ testRoad/dev/testAll.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+#curl -X GET -v http://localhost:8080/api/v1/namespace/pavedroad.io/usersLIST
+#curl -X GET -v http://localhost:8080/api/v1/namespace/pavedroad.io/users/12345
+#curl -X PUT -d "{}" -v http://localhost:8080/api/v1/namespace/pavedroad.io/users/12345
+#curl -X DELETE -d "{}" -v http://localhost:8080/api/v1/namespace/pavedroad.io/users/12345
